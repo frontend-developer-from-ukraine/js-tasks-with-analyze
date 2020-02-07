17806f8a060b0c1cce3b3d1c2a57aa739f31b079
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-02-07 05:40:38 -0800

    with algorithm

--- Lesson8-object/task1-creat-object/index.js
+++ Lesson8-object/task1-creat-object/index.js
@@ -1,4 +1,4 @@
-function transformToObject(arr) {
+const transformToObject = arr => {
     let obj = {};
 
     arr.forEach(el => {
@@ -6,3 +6,15 @@ function transformToObject(arr) {
     });
     return obj;
 }
+
+const arr = [1, 'text'];
+console.log(transformToObject(arr));
+
+/*
+Алгоритм
+1. создали стелочную функцию , в аргумент передали массив 
+2. объявили новый объект 
+3. перебрали объект , call back функция 
+4. обьект массиву равен не массиву
+5. вернуть переменную 
+*/
